4aa9348c243064263ecb0352e962479b03e125f7
Akanni Emmanuel <akannie33@gmail.com>
2022-11-22 13:43:54 +0100

    solution to task 0

--- _printf.c
+++ _printf.c
@@ -1,21 +1,35 @@
 #include "main.h"
+
+/**
+ * _print_str - prints a string to the screen
+ * @str: string given
+ * Return: void
+ */
+int _print_str(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i++]);
+	}
+
+	return (i);
+}
+
 /**
   * _printf - print the character to the stdout
-  * @str: string to print the characters
+  * @fmt: string to print the characters
   * @...: varaidic parameters (unknown)
   *
   * Return: number of characters printed
   */
-int _printf(const char *str, ...)
+int _printf(const char *fmt, ...)
 {
 	va_list args;
-	int num;
-	char c;
 	char *s;
 	int len = _strlen(fmt);
-	int i = 0;
-	int count = 0;
-	int value = 0;
+	int i = 0, count = 0, value = 0, num = 0;
 
 	va_start(args, fmt);
 	while (fmt[i])
@@ -27,34 +41,25 @@ int _printf(const char *str, ...)
 				case 's':
 					i++;
 					s = va_arg(args, char *);
-					while (*s)
-						_putchar(*s++);
+					count += _print_str(s) - 1;
 					break;
 				case 'c':
 					i++;
-					c = (char)va_arg(args, int);
-					_putchar(c);
+					_putchar((char)va_arg(args, int));
+					count += 1;
 					break;
 				default:
 					_putchar(fmt[i]);
+					count += 1;
 					break;
 			}
 		}
 		else
+		{
+			count += 1;
 			_putchar(fmt[i]);
+		}
 		i++;
-
-		if (fmt[i + 1] == '\0')
-		{
-			break;
-		}
-		if (fmt[i + 1] != '\0')
-		{
-			value = write(1, &fmt[i + 1], 1);
-			count = count + value;
-			i = i + 2;
-			continue;
-		}
 	}
 	va_end(args);
 	return (count);
